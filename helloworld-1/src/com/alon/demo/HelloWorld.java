56ccd34efc7b05d4521b2b7897d9565ca8094ba6
Alon.Du <alon.du@hotmail.com>
2017-08-15 13:32:26 +0800

    hello world

--- helloworld-1/src/com/alon/demo/HelloWorld.java
+++ helloworld-1/src/com/alon/demo/HelloWorld.java
@@ -2,8 +2,12 @@ package com.alon.demo;
 
 public class HelloWorld {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.print("Hello World");
     }
 }
+
+
+
+
+
